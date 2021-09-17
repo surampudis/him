924f4c97b4100f8d038e6c6b0b3c511015ed1cf4
shanthi surampudi <ss175@my.tamuct.edu>
2021-09-17 06:32:25 -0500

    iib_changes

---
+++ js/course.js
@@ -0,0 +1,7 @@
+function topicslist() {
+    if (document.getElementById("contentlist").style.display == "none") {
+        document.getElementById("contentlist").style.display = "block";
+    } else {
+        document.getElementById("contentlist").style.display = "none";
+    }
+}
