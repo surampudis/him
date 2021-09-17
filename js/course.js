57c42b5ce855e18f037381bff3ff2d2e27b33c56
shanthi surampudi <ss175@my.tamuct.edu>
2021-09-17 18:52:47 -0500

    basic-done

--- js/course.js
+++ js/course.js
@@ -13,3 +13,23 @@ function benefitlist() {
         document.getElementById("traininglist").style.display = "none";
     }
 }
+
+function contactButton() {
+
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var subject = document.getElementById("subject").value;
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "hireintomnc@gmail.com",
+        Password: "hireinto@123",
+        To: 'hrconnect@himsoftware.com',
+        From: "hireintomnc@gmail.com",
+        Subject: fname + " " + lname + " sent a new message from website",
+        Body: subject,
+    }).then(
+        message => alert("mail sent successfully. Thak you " + fname + " for your interest. Click on HOME in Menu to return to home page")
+
+    );
+
+}
